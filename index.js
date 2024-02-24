1f3da422dac69499a486a68daf4434f984cf319a
Sh-Farhan <farhan.mh.shaikh@gmail.com>
2024-02-24 13:20:35 +0530

    second

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { products } from "./db/products.js";
 const productContainer = document.getElementById("products");
 
 for(let product of products){
-    let cardContainer = document.createElement("div");
+    const cardContainer = document.createElement("div");
     cardContainer.classList.add(
         "card",
         "card-vertical",
@@ -13,7 +13,7 @@ for(let product of products){
         "relative",
         "shadow"
     );  
-    let imageContainer = document.createElement("div");
+    const imageContainer = document.createElement("div");
     imageContainer.classList.add("card-image-container");
 
     const image = document.createElement("image");
@@ -22,8 +22,11 @@ for(let product of products){
     image.setAttribute("alt",product.name);
     imageContainer.appendChild(image);
 
+    cardContainer.appendChild(imageContainer); 
+
     const cardDetailsContainer = document.createElement("div");
     cardDetailsContainer.classList.add("card-details");
+
     const brandContainer = document.createElement("div");
     brandContainer.classList.add("card-title");
     brandContainer.innerText = product.brand;
@@ -38,13 +41,48 @@ for(let product of products){
     descriptionContainer.appendChild(name);
 
     const price = document.createElement("p");
-    price.classList.add("card-price");
+    price.classList.add("card-price","d-flex","align-end","gap-sm");
     price.innerText = `Rs. ${product.price}`;
 
     const oldPrice = document.createElement("span");
     oldPrice.classList.add("price-strike-through");
     oldPrice.innerText = `Rs. ${product.oldPrice}`;
     price.appendChild(oldPrice);
+
+    const discount = document.createElement("span");
+    discount.classList.add("discount");
+    discount.innerText =  `(${product.discount}% OFF)`;
+    price.appendChild(discount);
+
+    descriptionContainer.appendChild(price);
+
+    const ratings = document.createElement("p");
+    ratings.classList.add("d-flex","align-center");
+
+    const star = document.createElement("span");
+    star.classList.add("material-icons-outlined"); 
+    star.innerText = "star";
+    ratings.appendChild(star);
+
+    descriptionContainer.appendChild(ratings);
+
+    cardDetailsContainer.appendChild(descriptionContainer);
+
+    // const ctaButton = document.createElement("div");
+    // ctaButton. classList.add("cta-btn");
+    // const cartButton = document.createElement("button");
+    // cartButton.classList.add("button", "btn-primary", "btn-icon"," cart-btn", "d-flex", "align-center", "justify-center", "gap", "cursor", "btn-margin");
+
+    // const cart = document.createElement("span");
+    // cart.classList.add("material-icons-outlined"); 
+    // cart.innerText = "shopping_cart";
+    // cartButton.appendChild(cart);
+    // cartButton.innerText = "Add to cart";
+    // ctaButton.appendChild(cartButton);
+    // cardDetailsContainer.appendChild(ctaButton);
+
+    // cardContainer.appendChild(imageContainer);  
+    cardContainer.appendChild(cardDetailsContainer);
 
 
     productContainer.appendChild(cardContainer);
